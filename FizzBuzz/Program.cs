0c56643a5115538fcdb918c3e962ea270445b0af
LornaGrajek <lorna.grajek@gmail.com>
2021-12-14 20:00:58 -0800

    finished the fizzbuzz project again

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("FizzBuzz");
+string input = "";
+int number = 0;
+
+do
+{
+    Console.WriteLine("Please enter a positive integer");
+    input = Console.ReadLine();
+    number = int.Parse(input);
+
+    if (number <= 0)
+    {
+        Console.WriteLine("The number should be greater than 0");
+    }
+} while (number <= 0);
+
+for (int i = 0; i <= number; i++)
+{
+    if ((i % 3) <= 0)
+     {
+         if ((i % 5) <= 0)
+         {
+             Console.WriteLine("Buzz");
+         }
+         else
+         {
+             Console.WriteLine("Fizz");
+         }
+     }
+     else
+     {
+         Console.WriteLine(i);
+     }
+}
